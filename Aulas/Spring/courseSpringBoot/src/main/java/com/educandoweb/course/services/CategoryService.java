ae2191573f160b15a3b889a4d95ed563104f61ac
Andrew <53877651+AndrewMarques2018@users.noreply.github.com>
2021-10-03 10:04:46 -0300

    User insert

--- Aulas/Spring/courseSpringBoot/src/main/java/com/educandoweb/course/services/CategoryService.java
+++ Aulas/Spring/courseSpringBoot/src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.educandoweb.course.entits.Category;
 import com.educandoweb.course.repositories.CategoryRepository;
@@ -15,14 +14,14 @@ public class CategoryService {
 
 	@Autowired
 	private CategoryRepository repository;
-	
-	public List<Category> findAll () {
+
+	public List<Category> findAll() {
 		return repository.findAll();
 	}
-	
-	public Category findById (long id) {
+
+	public Category findById(long id) {
 		Optional<Category> obj = repository.findById(id);
 		return obj.get();
 	}
-	
+
 }
--- Aulas/Spring/courseSpringBoot/src/main/java/com/educandoweb/course/services/UserService.java
+++ Aulas/Spring/courseSpringBoot/src/main/java/com/educandoweb/course/services/UserService.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
 import com.educandoweb.course.entits.User;
 import com.educandoweb.course.repositories.UserRepository;
@@ -15,14 +14,18 @@ public class UserService {
 
 	@Autowired
 	private UserRepository repository;
-	
-	public List<User> findAll () {
+
+	public List<User> findAll() {
 		return repository.findAll();
 	}
-	
-	public User findById (long id) {
+
+	public User findById(long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
 	}
 	
+	public User insert (User obj) {
+		return repository.save(obj);
+	}
+
 }
